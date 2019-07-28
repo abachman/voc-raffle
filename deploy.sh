c677c89d6e36df50e2aeacf248893f53affe8055
Adam Bachman <adam.bachman@gmail.com>
2019-07-28 09:06:19 -0400

    copypasted a deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 echo 'subtree'
-git subtree push --prefix dist origin gh-pages
-echo 'push'
-git push origin gh-pages --force
+git subtree split --prefix dist -b gh-pages # create a local gh-pages branch containing the splitted output folder
+git push -f origin gh-pages:gh-pages # force the push of the gh-pages branch to the remote gh-pages branch at origin
+git branch -D gh-pages # delete the local gh-pages because you will need it: ref
