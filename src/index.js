b39fdb3efdb292a770eca1625649d884cf84da12
Adam Bachman <adam.bachman@gmail.com>
2019-08-01 13:39:37 -0400

    pre-hash emails

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
-import md5 from 'js-md5'
 
 import './core-ext/each_slice'
 import './core-ext/shuffle'
@@ -28,27 +27,27 @@ function useInterval(callback, delay) {
 }
 
 const PEOPLE = [
-  [ 'Abdullah', 'abdullahbudri@gmail.com', ],
-  [ 'Adam', 'adam.bachman@gmail.com' ],
-  [ 'Bransyn', 'bransynluther@gmail.com', ],
-  [ 'Brendan', 'brendanbeltz@gmail.com', ],
-  [ 'Gabe', 'gabezurita@gmail.com', ],
-  [ 'Jamie', 'jgaskins@gmail.com', ],
-  [ 'Jeesoo', 'ysujs951@gmail.com', ],
-  [ 'Jess', 'jessbeach1@gmail.com', ],
-  [ 'Jessica', 'jmdembe@gmail.com', ],
-  [ 'Mae', 'maebeale@gmail.com', ],
-  [ 'Miranda', 'msw3bb@virginia.edu', ],
-  [ 'Paige', 'paigelfink@gmail.com', ],
-  [ 'Rachel', 'rachellesleywyatt@gmail.com', ],
-  [ 'Robb', 'robb@thekidds.org', ],
-  [ 'Svetlana', 'svileshina@gmail.com', ],
+  [ 'Abdullah', '1968d12d3a8152c00486b69dd2208c2a' ],
+  [ 'Adam', '939848268c73b4049f90ee0703df7c68' ],
+  [ 'Bransyn', '295744ee9f01c7df6f14415198c709d2' ],
+  [ 'Brendan', '2951ba18bfbc833b11008fad2185d8ae' ],
+  [ 'Gabe', 'bf553ec76f2b6255deb47d9d8633df64' ],
+  [ 'Jamie', '76de16cb97b382f990c4798277e37bab' ],
+  [ 'Jeesoo', 'cdb94ae3973257571d99e66be1ab0894' ],
+  [ 'Jess', '6254a7c32291e428e3138ef98ec18f75' ],
+  [ 'Jessica', '35ef6db3a9ee1a2f0bc71fe0803966b4' ],
+  [ 'Mae', '2fae245f92a4c73daaa972ae6dccb2fb' ],
+  [ 'Miranda', 'f6e1b9f0339ccfdd559744d2f7cf7166' ],
+  [ 'Paige', '9e451e3cc242a8b40e5af81890b248b0' ],
+  [ 'Rachel', '3e4ea8302fc4154e17be4bfaa9e30e0f' ],
+  [ 'Robb', '6340bd48e9f1248c6344d3008c0a08d7' ],
+  [ 'Svetlana', '8d697b8ed37d4c477bd7f6a207ef1547' ]
 ]
 
 PEOPLE.shuffle()
 
 const Image = ({ email }) => {
-  const imgUrl = `https://www.gravatar.com/avatar/${md5(email)}?d=monsterid`
+  const imgUrl = `https://www.gravatar.com/avatar/${email}?d=monsterid`
   return (
     <img src={imgUrl} />
   )
@@ -72,8 +71,6 @@ const Row = ({ people }) => {
 }
 
 const App = () => {
-  console.log('hey from in an app')
-
   const [ people, setPeople ] = useState(PEOPLE);
   const [ isRunning, setIsRunning ] = useState(false);
 
@@ -81,13 +78,11 @@ const App = () => {
     if (isRunning && people.length > 1) {
       const nextPeople = people.slice()
       nextPeople.random_pop()
-      console.log("PEOPLE", nextPeople)
       setPeople(nextPeople)
     }
   }, 500)
 
   let table = []
-  console.log("rebuild table with", people)
   people.eachSlice(4, (slice) => table.push(<Row people={slice} key={slice[0][0]} />))
 
   return (
